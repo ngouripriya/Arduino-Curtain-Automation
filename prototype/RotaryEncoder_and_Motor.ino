84271a46eaeb5f3e71aa4b55865e05461abeccd6
gouripri <149901519+gouripri@users.noreply.github.com>
2024-10-31 20:26:32 -0400

    Rotary Encoder Controls the Motor

---
+++ prototype/RotaryEncoder_and_Motor.ino
@@ -0,0 +1,82 @@
+#define CLK 2
+#define DT 3
+//#define SW 4
+const int dirPin =8;
+const int stepPin = 9;
+const int stepsPerRevolution =200;
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir ="";
+//unsigned long lastButtonPress = 0;
+
+void setup() {
+        
+	// Set encoder pins as inputs
+	pinMode(CLK,INPUT);
+	pinMode(DT,INPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+//	pinMode(SW, INPUT_PULLUP);
+
+	// Setup Serial Monitor
+	Serial.begin(9600);
+
+	// Read the initial state of CLK
+	lastStateCLK = digitalRead(CLK);
+}
+
+void loop() {
+        
+	// Read the current state of CLK
+	currentStateCLK = digitalRead(CLK);
+
+	// If last and current state of CLK are different, then pulse occurred
+	// React to only 1 state change to avoid double count
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+
+		// If the DT state is different than the CLK state then
+		// the encoder is rotating CCW so decrement
+		if (digitalRead(DT) != currentStateCLK) {
+			counter --;
+			currentDir ="CCW";
+      digitalWrite(dirPin, LOW);
+      for(int x=0; x< 5; x++){
+        digitalWrite(stepPin, HIGH);
+		    delayMicroseconds(1000);
+		    digitalWrite(stepPin, LOW);
+		    delayMicroseconds(1000);
+      }
+		} else {
+			// Encoder is rotating CW so increment
+			counter ++;
+			currentDir ="CW";
+      digitalWrite(dirPin, HIGH);
+      for(int x=0; x< 5; x++){
+        digitalWrite(stepPin, HIGH);
+		    delayMicroseconds(1000);
+		    digitalWrite(stepPin, LOW);
+		    delayMicroseconds(1000);
+      }
+      
+      delay(1);
+
+
+
+		}
+
+		Serial.print("Direction: ");
+		Serial.print(currentDir);
+		Serial.print(" | Counter: ");
+		Serial.println(counter);
+
+
+	}
+
+	// Remember last CLK state
+	lastStateCLK = currentStateCLK;
+
+	// Put in a slight delay to help debounce the reading
+	delay(1);
+}
