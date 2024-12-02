11ccc4f45de9ed77a195ca238394e240a608c7dc
gouripri <149901519+gouripri@users.noreply.github.com>
2024-12-02 11:56:57 -0500

    Perfection

--- Final/Automatic.cpp
+++ Final/Automatic.cpp
@@ -17,15 +17,18 @@ void modeAutomatic(){
   scrolling =0;
 
   while (scrolling ==0) {
+    delay(50);
     int currentIndoor = indoor();
     int currentOutdoor = outdoor();
     int pref = preference();
-    Serial.print("current = ");
+/*    Serial.print("current = ");
     Serial.println(currentIndoor);
     Serial.print("Outdoor = ");
     Serial.println(currentOutdoor);
     Serial.print("pref = ");
     Serial.println(pref);
+    */
+
     //too dark inside and brighter outside
     if (currentIndoor < pref - 20 && currentIndoor < currentOutdoor && getPos() > getMin()) {
       Serial.println("In 1");
@@ -43,43 +46,64 @@ void modeAutomatic(){
       Serial.println("In 3");
       autoOpen();
     }
+    else{
+      updatePast(0);
+    }
+    delay(30);
     if(click()){
+      Serial.println("hello");
+
       scrolling = 5;
       //invalid, should exit
     }
 
-    delay(5000);
+    delay(50);
   }
 
 }
 
 void autoOpen() {
-  digitalWrite(dirPin, HIGH);
+  int current = getPos();
+  int max = getMax();
+  int currPos = getPos();
 
   if (!repeating(1)) {
-    for(int x=0; x< 10 && getPos() <=  getMax() - 1; x++){
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(1000);
-      updatePos(1);
-    }
+      if(current+10 <= max){
+        digitalWrite(dirPin, HIGH);
+        for(int x=0; x< 10; x++){
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(1000);
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(1000);
+        }
+        current = current+10;
+        updatePos(10);
+        delay(10);
+      }
+
   }
   updatePast(1);
 }
 
 void autoClose() {
-  digitalWrite(dirPin, LOW);
+  int min = getMin();
+  int current = getPos();
+  if (!repeating(1)) {
+    digitalWrite(dirPin, LOW);  
+    if(current - 10 >= min){
+      digitalWrite(dirPin, LOW);
+      for(int x=0; x< 10; x++){
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(1000);
 
-  if (!repeating(-1)) {
-    for(int x=0; x< 10 && getPos() > getMin() - 1; x++){
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(1000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(1000);
-      updatePos(1);
-    }
-  }
+      }     
+      current-=10;
+      updatePos(-10);
+      delay(10);
+    }  
+  }    
   updatePast(-1);
 }
 
